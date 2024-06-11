ac15d64b964ec70728339dce3ceaed62b39c227e
gbenga bamidele <kanmigbenga77@gmail.com>
2024-06-11 11:59:44 -0400

    Display Colors

--- example/01-sample.sh
+++ example/01-sample.sh
@@ -6,5 +6,12 @@ echo start nginx
 
 # print colors
 
+echo -e "\e[31mHello in Red Color\e[0m"
+echo -e "\e[32mHello in Green Color\e[0m"
+echo -e "\e[33mHello in Yellow Color\e[0m"
+echo -e "\e[34mHello in Blue Color\e[0m"
+echo -e "\e[35mHello in Magenta Color\e[0m"
+echo -e "\e[36mHello in Cyan Color\e[0m"
+
 
 
