a9c5f1bce077226fe1076f1c302524626d1f9ea0
gbenga bamidele <kanmigbenga77@gmail.com>
2024-06-11 22:32:55 -0400

    if condition

--- example/04-ifcond.sh
+++ example/04-ifcond.sh
@@ -4,6 +4,10 @@ read -p 'Enter your age' age
 
 if [ "${age}" -lt 18 ]; then
   echo you are minor
+
+  elif [ "${age}" -gt 60 ]; then
+    echo you are senior citizen
+      
   else
 
     echo you are major
